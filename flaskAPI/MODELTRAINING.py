0ba076d5dc82d2540335da0538fe0c4be62b1f0e
Divvy-Saran <107314651+Divvy-Saran@users.noreply.github.com>
2023-05-13 16:36:18 -0400

    Update MODELTRAINING.py

--- flaskAPI/MODELTRAINING.py
+++ flaskAPI/MODELTRAINING.py
@@ -4,8 +4,8 @@ transform = transforms.Compose([
     transforms.Normalize((0.5,), (0.5,))
 ])
 
-train_dataset = datasets.ImageFolder(os.path.join(dataset_path, 'asl_alphabet_train'), transform=transform)
-test_dataset = datasets.ImageFolder(os.path.join(dataset_path, 'asl_alphabet_test'), transform=transform)
+train_dataset = datasets.../archive/asl_alphabet_train/asl_alphabet_train(os.path.join(dataset_path, 'asl_alphabet_train'), transform=transform)
+test_dataset = datasets.../archive/asl_alphabet_train/asl_alphabet_train(os.path.join(dataset_path, 'asl_alphabet_test'), transform=transform)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
