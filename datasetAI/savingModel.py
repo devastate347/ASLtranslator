c8d72a53d085e21e2bb9aa800c626a157bb65644
Ayaan Sachdeva <ayaansachdeva1000@gmail.com>
2023-05-13 12:51:04 -0400

    added folders

---
+++ datasetAI/savingModel.py
@@ -0,0 +1,5 @@
+from pyexpat import model
+import torch
+
+PATH = 'asl_model.pth' 
+torch.load(model.state_dict(), asl_model.pth)
--- savingModel.py
+++
@@ -1,5 +0,0 @@
-from pyexpat import model
-import torch
-
-PATH = '/home/ayaans/Documents/ASLtest/savedModel.txt' 
-torch.save(model.state_dict(), asl_model.pth)
